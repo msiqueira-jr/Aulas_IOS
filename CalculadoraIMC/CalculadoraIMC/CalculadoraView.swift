4cb31444758b79e68c679a7c3998776d3f792afc
Mauro <mauro.jr@hotmail.fr>
2020-08-06 01:17:02 -0300

    first commit

---
+++ CalculadoraIMC/CalculadoraIMC/CalculadoraView.swift
@@ -0,0 +1,21 @@
+//
+//  CalculadoraView.swift
+//  CalculadoraIMC
+//
+//  Created by Mauro Siqueira Jr on 31/07/20.
+//  Copyright © 2020 JuniorMauro. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class CalculadoraView: UIView {
+
+    
+//    func setupView() {
+//        self.alturaTextFild.palceholder = "Altura"
+//        self.PesoTextFild.palceholder = "Altura"
+//    }
+//
+
+}
